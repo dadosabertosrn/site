8dd50a061aa3ee49913e97a9f0b67a0120a2ecbd
joao paulo <joaopaulopmedeiros@gmail.com>
2020-05-11 19:52:28 -0300

    página de notícias

---
+++ js/eventos.js
@@ -0,0 +1,24 @@
+const sectionCards = document.querySelector(".eventos .container");
+const card = document.querySelector(".item");
+
+const conteudos = [
+  {
+    title: "Título de evento 1",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.",
+    conteudo_id: "GykTLqODQuU"
+  },
+  {
+    title: "Título de evento 2",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.",
+    conteudo_id: "GykTLqODQuU"
+  },
+];
+
+conteudos.map(element => {
+  const cardClone = card.cloneNode(true)
+  cardClone.querySelector(".title").innerHTML = element.title
+  cardClone.querySelector(".description").innerHTML = element.description
+  sectionCards.appendChild(cardClone)
+})
+
+card.remove()
